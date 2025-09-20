f9028efa184c11b843b534abdf48e83cae569011
isc777 <126932176+isc777@users.noreply.github.com>
2025-09-21 02:30:27 +0800

    Update RewardsPage.jsx

--- src/RewardsPage.jsx
+++ src/RewardsPage.jsx
@@ -123,9 +123,9 @@ function RewardsPage({ lang }) {
             <div
               style={{
                 alignItems: "center",
-                padding: "1px 15px",
+                //padding: "1px 15px",
                 height: 50,
-                width: 200,
+                width: 350,
                 backgroundColor: "#eee",
                 borderRadius: 15,
                 overflow: "hidden",
