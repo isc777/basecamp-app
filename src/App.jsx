267bf74d5f2af7e0d1170de34ec0fb99930997a0
林家緯 <iscole0585@gmail.com>
2025-09-20 19:21:04 +0800

    bus

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import SettingPage from "./SettingPages";
 const menuItems = {
   zh: [
     { title: "探索公司環境", path: "/explore" },
-    { title: "認識公司資訊", path: "/info" },
+    { title: "交通資訊", path: "/info" },
     { title: "社交任務", path: "/social" },
     { title: "獎勵兌換", path: "/rewards" },
     { title: "設定與個人資訊", path: "/settings" },
